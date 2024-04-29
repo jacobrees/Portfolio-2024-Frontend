8189d2070d463d1e419d2810c7e1a105a363f1fd
jacobrees <jacob.rees97@yahoo.com>
2024-04-29 07:04:02 +0100

    Make mobile nav close when user clicks a link

--- main.js
+++ main.js
@@ -3,6 +3,13 @@ import "./style.scss";
 const openNavButton = document.querySelector(".navigation-menu");
 const mobileNav = document.querySelector(".mobile-nav");
 const closeNavButton = document.querySelector(".mobile-close-navbar");
+const mobileNavLinks = document.querySelectorAll(".mobile-nav-link");
+
+mobileNavLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    mobileNav.classList.remove("open");
+  });
+});
 
 openNavButton.addEventListener("click", () => {
   mobileNav.classList.add("open");
