c627fdffe76681e4c9151469a14812b4c1480dbf
jacobrees <jacob.rees97@yahoo.com>
2024-04-29 06:54:33 +0100

    remove scrolling function in javascript add functionality to download resume

--- main.js
+++ main.js
@@ -1,37 +1,8 @@
 import "./style.scss";
 
-const navLinks = document.querySelectorAll(".navbar-link");
-const mobileNavLinks = document.querySelectorAll(".mobile-nav-link");
 const openNavButton = document.querySelector(".navigation-menu");
 const mobileNav = document.querySelector(".mobile-nav");
 const closeNavButton = document.querySelector(".mobile-close-navbar");
-
-navLinks.forEach((navLink) => {
-  navLink.addEventListener("click", (e) => {
-    e.preventDefault();
-    const id = navLink.textContent.toLowerCase();
-    const section = document.getElementById(id);
-    const position = section.offsetTop;
-    window.scrollTo({
-      top: position,
-      behavior: "smooth",
-    });
-  });
-});
-
-mobileNavLinks.forEach((navLink) => {
-  navLink.addEventListener("click", (e) => {
-    e.preventDefault();
-    const id = navLink.textContent.toLowerCase();
-    const section = document.getElementById(id);
-    const position = section.offsetTop;
-    window.scrollTo({
-      top: position,
-      behavior: "smooth",
-    });
-    mobileNav.classList.remove("open");
-  });
-});
 
 openNavButton.addEventListener("click", () => {
   mobileNav.classList.add("open");
